0e7bc74190a2b109985d4675b39526058de4bedc
jin <kjin7612@gmail.com>
2019-04-13 20:36:09 -0400

    190413_hello_html

---
+++ src/main/java/com/jtool/rest/config/ThymeleafConfiguration.java
@@ -0,0 +1,74 @@
+package com.jtool.rest.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import java.io.Serializable;
+
+@Configuration
+@Slf4j
+public class ThymeleafConfiguration implements Serializable{
+//	private static final long serialVersionUID = SpringSecurityCoreVersion.SERIAL_VERSION_UID;
+//	private Logger logger = LoggerFactory.getLogger(ThymeleafConfiguration.class);
+
+	public ThymeleafConfiguration() {}
+	
+	@Bean
+    public ViewResolver viewResolver() {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        resolver.setCharacterEncoding("UTF-8");
+        log.error("ThymeleafConfiguration - viewResolver");
+        
+        return resolver;
+    }
+	
+	@Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setTemplateResolver(templateResolver());
+        log.error("ThymeleafConfiguration - templateEngine");
+        
+        return engine;
+    }
+
+    @Bean
+    public ITemplateResolver templateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setCharacterEncoding("UTF-8");
+        log.error("ThymeleafConfiguration - templateResolver");
+        
+        return resolver;
+    }
+    
+	@Bean
+    public SpringTemplateEngine emailTemplateEngine() {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setTemplateResolver(emailTemplateResolver());
+        log.error("ThymeleafConfiguration - emailTemplateEngine");
+        return engine;
+    }
+
+    @Bean
+    public ClassLoaderTemplateResolver emailTemplateResolver() {
+        ClassLoaderTemplateResolver emailTemplateResolver = new ClassLoaderTemplateResolver();
+        emailTemplateResolver.setPrefix("/mails/");
+        emailTemplateResolver.setSuffix(".html");
+        emailTemplateResolver.setTemplateMode(TemplateMode.HTML);
+        emailTemplateResolver.setCharacterEncoding("UTF-8");
+        log.error("ThymeleafConfiguration - emailTemplateResolver");
+        
+       return emailTemplateResolver;
+    }
+}
---
+++ src/main/java/com/jtool/rest/config/WebConfiguration.java
@@ -0,0 +1,56 @@
+package com.jtool.rest.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.io.Serializable;
+
+@Configuration
+@EnableWebMvc
+@Slf4j
+public class WebConfiguration extends WebMvcConfigurerAdapter implements Serializable{
+//	private static final long serialVersionUID = SpringSecurityCoreVersion.SERIAL_VERSION_UID;
+//	private Logger logger = LoggerFactory.getLogger(WebConfiguration.class);
+
+
+	public WebConfiguration() {	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                "/webjars/**"
+                , "/img/**"
+                , "/css/**"
+                , "/js/**"
+                , "/bower_components/**"
+        		)
+        .addResourceLocations(
+                "classpath:/META-INF/resources/webjars/"
+                , "classpath:/static/img/"
+                , "classpath:/static/css/"
+                , "classpath:/static/js/"
+                , "classpath:/static/bower_components/"
+        		)
+        .addResourceLocations(
+                "classpath:/static/img/**"
+                , "classpath:/static/css/**"
+                , "classpath:/static/js/**"
+                , "classpath:/static/bower_components/**"
+        		);
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/index").setViewName("index");
+        
+//        logger.error("addViewControllers Error");
+		log.error("addViewControllers Error");
+	}
+}
